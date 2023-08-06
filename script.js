207bdbf1392b3732d4c761ec42a991de5866987d
Red-K4tana <86315447+Red-K4tana@users.noreply.github.com>
2023-08-06 21:40:48 +0300

    append...

--- script.js
+++ script.js
@@ -11,16 +11,16 @@ class LinkedListNode {
   // если придет callback, то он разберет this.val на свойства и выведет их; если не callback, выведет просто this.val
   // то есть мы должны знать, является ли this.val объектом, прежде чем добавлять аргумент callback
   toString (callback) {
-    console.log( callback ? callback(this.val) : `${this.val}` )
+    // console.log( callback ? callback(this.val) : `${this.val}` )
     return callback ? callback(this.val) : `${this.val}`
   }
 }
 
-let node01 = new LinkedListNode('first')
-let node02 = new LinkedListNode({value: 'ewq', key: 231})
+// let node01 = new LinkedListNode('first')
+// let node02 = new LinkedListNode({value: 'ewq', key: 231})
 
-node01.toString()
-node02.toString(nodeStringifier) // подставляю callback, так как node02.val это объект
+// node01.toString()
+// node02.toString(nodeStringifier) // подставляю callback, так как node02.val это объект
 
 
 class LinkedList {
@@ -29,8 +29,8 @@ class LinkedList {
     this.tail = null
   }
 
-  prepend(value) {
-    const newNode = LinkedListNode(value, this.head)
+  prepend (value) {
+    const newNode = new LinkedListNode(value, this.head)
     this.head = newNode
 
     if (!this.tail) this.tail = newNode
@@ -38,4 +38,21 @@ class LinkedList {
     console.log('prepend ', this)
     return this
   }
+  append (value) {
+    const newNode = new LinkedListNode(value)
+    this.tail = newNode
+
+    if (!this.head) this.head = newNode
+
+    console.log('append ', this)
+    return this
+  }
 }
+
+let linkedList01 = new LinkedList()
+linkedList01.prepend('node01')
+linkedList01.append('node02')
+linkedList01.prepend('node03')
+
+
+
